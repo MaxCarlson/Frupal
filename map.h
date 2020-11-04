f956dcf7e420bdcbe53656dd2bd5871e7cab89ac
MaxCarlson <bart4282819@gmail.com>
2020-11-04 15:23:37 -0800

    Changed map squares a bit.

--- map.h
+++ map.h
@@ -1,16 +1,23 @@
 #pragma once
 #include "camera.h"
 
+enum Obstacle
+{
+    NONE = 0,
+    TREE,
+    BOULDER,
+};
 
 
 struct MapSquare
 {
     char symbol;
     uint8_t color;
+    Obstacle obstacle;
 
     MapSquare() = default;
-    MapSquare(char symbol, uint8_t color) 
-        : symbol{symbol}, color{color}
+    MapSquare(char symbol, uint8_t color, Obstacle obstacle) 
+        : symbol{symbol}, color{color}, obstacle{obstacle}
     {}
 };
 
