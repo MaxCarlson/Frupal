44fa9a9421ebb3cc5e371cbc082241c4503637ff
MaxCarlson <bart4282819@gmail.com>
2020-11-05 14:53:34 -0800

    Added the absolute basics of the Player class.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,20 @@ int main()
 
     std::cout << "Hello";
 
+    // Start screen
+    // Load map
+    // Generate hints
+    
+
+    // Main Loop
+    //
+    // Check for input
+    // Print map
+    // Print UI
+    // Apply user input
+    // Run any other systems
+    // Refresh
+
  
     return 0;
 }
--- player.h
+++ player.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "tool.h"
+#include <vector>
+
+
+
+class Player
+{
+    int energy;
+    int money;
+    std::vector<Tool*> tools;
+public:
+    Player() :
+        energy{100}, money{1000}, tools{}
+    {} 
+};
