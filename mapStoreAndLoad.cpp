610584e534681bac277cfa98434f55dc9bdcddcc
Devon Berry <devberry@babbage.cs.pdx.edu>
2020-11-26 10:48:08 -0800

    Deleted stuff

--- mapStoreAndLoad.cpp
+++ mapStoreAndLoad.cpp
@@ -20,25 +20,6 @@ bool mapStoreAndLoad::save(Map& map, const std::string fileName) {
     outFile << map.getWidth() << " ";
     outFile << map.getHeight() << "\n";
 
-    for(int i = 0; i < map.getHeight(); ++i)
-    {
-        for(int j = 0; j < map.getWidth(); ++j) {
-          MapSquare squareToSave = map.sq(i, j);
-          //Save coordinates.
-          outFile << j << " " << i << " ";
-          outFile << squareToSave.discovered << " ";
-          //Save terrain type as an integer.
-          outFile << static_cast<int>(squareToSave.terrain) << " ";
-          //If there is an item on the square.
-          if(squareToSave.item != nullptr) {
-          outFile << squareToSave.item->getCh() << " ";
-          outFile << squareToSave.item->getName() << " ";
-          }
-          outFile << "\n";
-            //delete map[i][j].item;
-        }
-        //delete[] map[i];
-    }
     
   }
   else {
