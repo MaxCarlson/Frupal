ea609fea04ff22b22f8175b547cbb680817d03f5
Devon Berry <devberry@babbage.cs.pdx.edu>
2020-11-26 10:54:34 -0800

    Indented

--- mapStoreAndLoad.cpp
+++ mapStoreAndLoad.cpp
@@ -2,58 +2,50 @@
 #include <fstream>
 #include <iostream>
 
-//Constructor
-/*
-mapStoreAndLoad::mapStoreAndLoad(Map& map) {
-  this->map = map;
-}
-*/
-
-
 bool mapStoreAndLoad::save(Map& map, Player& player, const std::string fileName) {
   std::ofstream outFile;
   outFile.exceptions ( std::ofstream::failbit | std::ofstream::badbit );
   try {
-  outFile.open(fileName);
+    outFile.open(fileName);
 
-  if(outFile.is_open()) {
-    //Player info.
-    outFile << player.getEnergy() << " ";
-    outFile << player.getMoney() << " ";
-    outFile << player.getX() << " ";
-    outFile << player.getY() << " ";
-    outFile << player.getHasBinoculars() << " ";
-    outFile << player.getOnShip() << " ";
-    outFile << player.showPlayerDeath() << " ";
-    outFile << static_cast<int>(player.getDir()) << " ";
-    //Not sure if this works properly, should save all tools.
-    for(const auto &e : player.getTools()) outFile << e << " ";
-    outFile  << "\n";
+    if(outFile.is_open()) {
+      //Player info.
+      outFile << player.getEnergy() << " ";
+      outFile << player.getMoney() << " ";
+      outFile << player.getX() << " ";
+      outFile << player.getY() << " ";
+      outFile << player.getHasBinoculars() << " ";
+      outFile << player.getOnShip() << " ";
+      outFile << player.showPlayerDeath() << " ";
+      outFile << static_cast<int>(player.getDir()) << " ";
+      //Not sure if this works properly, should save all tools.
+      for(const auto &e : player.getTools()) outFile << e << " ";
+      outFile  << "\n";
 
-    //Map info.
-    outFile << map.getWidth() << " ";
-    outFile << map.getHeight() << "\n";
-    MapSquare squareToSave;
-    for(int i = 0; i < map.getHeight(); ++i)
-    {
+      //Map info.
+      outFile << map.getWidth() << " ";
+      outFile << map.getHeight() << "\n";
+      MapSquare squareToSave;
+      for(int i = 0; i < map.getHeight(); ++i)
+      {
         for(int j = 0; j < map.getWidth(); ++j) {
           squareToSave = map.sq(j, i);
           outFile << j << " " << i << " ";
           outFile << squareToSave.discovered << " ";
           outFile << static_cast<int>(squareToSave.terrain) << " ";
           if(squareToSave.item != nullptr) {
-          outFile << squareToSave.item->getCh() << " ";
-          outFile << squareToSave.item->getName() << " ";
+            outFile << squareToSave.item->getCh() << " ";
+            outFile << squareToSave.item->getName() << " ";
           }
           outFile  << "\n";
         }
+      }
     }
-  }
-  else {
-    std::cerr << "ERROR:" << fileName << " did not open.";
-  }
+    else {
+      std::cerr << "ERROR:" << fileName << " did not open.";
+    }
 
-  outFile.close();
+    outFile.close();
   }
   catch(std::ofstream::failure &e) {
     std::cerr << "Exception opening/reading/closing file\n";
