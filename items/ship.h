1e67442da29459374578bd13bba3ebd5dfcf171c
courtneyficker <cficker@pdx.edu>
2020-12-02 15:38:55 -0800

    Added cost to ship and updated getDescription

--- items/ship.h
+++ items/ship.h
@@ -3,8 +3,25 @@
 
 class Ship : public Item
 {
+    int cost;
+
 public:
     Ship(std::string name) 
-        : Item{'S', name}
+        : Item{'S', name, 50}               // Default cost
     {}
+
+    Ship(std::string name, int cost) 
+        : Item{'S', name}, cost{cost}       // Option for custom cost
+    {}
+
+    Item::StringTuple getDescription() const
+    {
+        std::string l1;
+        static const std::string cost - "> Cost: ";
+        l1 = cost + std::to_string(getCost());
+        
+        return StringTuple{l1, "", "", ""};
+    }
+
+    int getCost() const { return cost; }
 };
